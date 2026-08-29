155d411e60143ce5d2edb234713d3c98082afa16
Matt <mullermatiash@gmail.com>
2026-08-28 21:29:05 -0300

    Actualización route.ts GitHubProvider issuer (fix error de sign in con cuenta github)

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -7,6 +7,7 @@ const handler = NextAuth({
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      issuer: "https://github.com/login/oauth",
       authorization: {
         params: {
           scope: "read:org repo delete_repo"
